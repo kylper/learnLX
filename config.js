15f00f629e25e1ff04ef23e4407f9b4d17c0db33
Kyle Permuy <kyle@varninja.com>
2015-05-16 12:43:09 -0400

    Changes Configuration file.

---
+++ config.js
@@ -0,0 +1,12 @@
+var configuration = {
+  'title': '',
+  'description': '',
+  'admins': [],
+  'moderators': [],
+  'languagecode': '',
+  'website': '',
+  'secret': 'learnLX',
+  'database': 'mongodb://user:password&54;hostname:port/'
+}
+
+module.exports = configuration;
