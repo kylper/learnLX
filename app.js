e7b04c7469e84aff4b312d85d75aacb0265e2a75
Kyle Permuy <kyle@varninja.com>
2015-05-16 09:44:07 -0400

    Adds Mongoose and MongoDB connection info. Project now successfully connects to db.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
+var mongoose = require('mongoose');
 
 var indexRoute = require('./routes/index');
 var checkRoute = require('./routes/check');
@@ -10,13 +12,19 @@ var contentRoute = require('./routes/content');
 
 var app = express();
 
+mongoose.connect('mongodb://localhost');
 
+var dbc = mongoose.connection;
+dbc.on('error', console.error.bind(console, 'connection error:'));
+dbc.once('open', function (callback) {
+  console.log("Mongoose connected! - for debugging only");
+});
 
-function ensureAuth(req, res, next) {
+/* function ensureAuth(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   req.session.error = 'Please sign in!';
   res.redirect('/signin');
-}
+}*/
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,6 +32,20 @@ app.use(cookieParser());
 app.use(express.static('html'));
 app.use(passport.initialize());
 app.use(passport.session());
+
+var sess = {
+  secret: 'learnLX',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {}
+};
+
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1) // trust first proxy
+  sess.cookie.secure = true // serve secure cookies
+}
+
+app.use(session(sess));
 
 app.use('/', indexRoute);
 app.use('/success', checkRoute);
@@ -35,8 +57,10 @@ app.post('/login',
   function(req, res) {
     // If this function gets called, authentication was successful.
     // `req.user` contains the authenticated user.
-    res.redirect('/users/' + req.user.username);
-  });
+  res.redirect('/users/' + req.user.username);
+});
+
+// signup
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -63,5 +87,11 @@ app.use(function(err, req, res, next) {
     res.send(err.message);
 });
 
+process.on('SIGINT', function() {
+  mongoose.connection.close(function () {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
 
 module.exports = app;
