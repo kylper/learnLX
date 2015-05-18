9106af45eca634cfef2896cd81f15ef66e00a9e8
Kyle Permuy <kyle@varninja.com>
2015-05-17 23:12:29 -0400

    Adds more information to the user model.

--- models/users.js
+++ models/users.js
@@ -2,13 +2,26 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
+// Language and Proficiency Level Sub-document
+// lang - Language (ISO 639 Alpha 3 code - eng, fra, etc.)
+// code - Proficiency Level (A1, B2, C2, etc.)
+// comski - Completed Skills (Basic Vocab, etc.)
+// proski - In-Progress Skill (Vocab Unit 2)
+var langSchema = new Schema({
+  lang: String,
+  code: String,
+  comski: [String],
+  proski: String,
+});
+
 var userSchema = new Schema({
   id: Number,
   name: String,
   email: String,
   admin: Boolean,
   moderator: Boolean,
-  created: { type: Date, default: Date.now }
+  languages: [langSchema],
+  created: { type: Date, default: Date.now },
 });
 
 userSchema.plugin(passportLocalMongoose);
