c65405d6fb92b02a3c443d1793023316bd081e0d
Kyle Permuy <kyle@varninja.com>
2015-07-13 16:25:04 -0400

    Updating Routes
    To fit with the new focus of the app, the routes have been changed
    again.

---
+++ routes/courses.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+
+/* Course Lesson Details */
+router.get('/id/:uuid/:unit/:lesson', function(req, res, next) {
+  res.status(501);
+  res.json({ message: "Function under development.", "resource": "Details course lesson (using uuid)." });
+});
+
+router.get('/name/:name/:unit/:lesson', function(req, res, next) {
+  res.status(501);
+  res.json({ message: "Function under development.", "resource": "Details course lesson (using name)." });
+});
+
+/* Course Unit Details */
+router.get('/id/:uuid/:unit', function(req, res, next) {
+  res.status(501);
+  res.json({ message: "Function under development.", "resource": "Details course unit (using uuid). Lists all lessons in unit." });
+});
+
+router.get('/name/:name/:unit', function(req, res, next) {
+  res.status(501);
+  res.json({ message: "Function under development.", "resource": "Details course unit (using name). Lists all lessons in unit." });
+});
+
+/* Full Course Details */
+router.get('/id/:uuid', function(req, res, next) {
+  res.status(501);
+  res.json({ message: "Function under development.", "resource": "List course details (using uuid). Lists all units and lessons." });
+});
+
+router.get('/name/:name', function(req, res, next) {
+  res.status(501);
+  res.json({ message: "Function under development.", "resource": "List course details (using name). Lists all units and lessons." });
+});
+
+/* Update Full Course details */
+router.put('/id/:uuid', function(req, res, next){
+  res.status(501);
+  res.json({ message: 'Function under development.' });
+});
+
+router.put('/name/:name', function(req, res, next){
+  res.status(501);
+  res.json({ message: 'Function under development.' });
+});
+
+/* List all courses with details */
+router.get('/list', function(req, res, next) {
+  res.status(501);
+  res.json([
+    {
+      "message": "Function under development.",
+      "resource": "List all courses."
+    },
+    {
+      "id": "5662342",
+      "name": "Javascript",
+      "active_users": "563"
+    },
+    {
+      "id": "5662343",
+      "name": "Ruby",
+      "active_users": "7811"
+    },
+    {
+      "id": "5662342",
+      "name": "Python",
+      "active_users": "1201"
+    }
+  ]);
+});
+
+/* router.get('/', function(req, res, next) {
+  res.status(501);
+  res.json({ message: "Function under development." });
+}); */
+
+module.exports = router;
--- routes/functions.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-
-  toCap: function(word) {
-    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-  }
-
-};
