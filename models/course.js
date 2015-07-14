adf0205ebf0a1a3e414ce2a93073fcd40adbcdc1
Kyle Permuy <kyle@varninja.com>
2015-07-13 23:33:17 -0400

    Updates Courses Model
    The Courses Mongoose Model is mostly finished. It may or may not be
    changed at this point.

--- models/course.js
+++ models/course.js
@@ -1,19 +1,35 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var lessonSchema = new Schema({
+var evalSchema = new Schema({
+  exists: Boolean,
+  fileLocation: String,
+  assetsLocation: String
+});
 
+var lessonSchema = new Schema({
+  id: Schema.Types.ObjectId,
+  name: String,
+  content: String, // This will be a markdown document in the form of a link learnlx.com/public/course-assets/coursename-unitid-lessonid
 });
 
 var unitSchema = new Schema({
-
+  id: Schema.Types.ObjectId,
+  name: String,
+  description: String,
+  lessons: [ lessonSchema ],
+  eval: evalSchema
 });
 
 var courseSchema = new Schema({
-  id: Number,
-  
-
+  id: Schema.Types.ObjectId,
+  name: String,
+  description: String,
+  languages: String,
+  platforms: String,
+  units: [ unitSchema ],
+  studentCount: Number
 });
 
-var Skill = mongoose.model('Skill', skillSchema);
-module.exports = Skill;
+var Course = mongoose.model('Course', courseSchema);
+module.exports = Course;
